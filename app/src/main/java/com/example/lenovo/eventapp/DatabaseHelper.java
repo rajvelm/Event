27d2f75bfb3dd3e43cc3f9a76dd9a57f0ad3144a
rajvelm <m.rajvel@gmail.com>
2016-10-27 09:47:38 +0530

    members tables created. login with email

--- app/src/main/java/com/example/lenovo/eventapp/DatabaseHelper.java
+++ app/src/main/java/com/example/lenovo/eventapp/DatabaseHelper.java
@@ -24,25 +24,96 @@ public class DatabaseHelper extends SQLiteOpenHelper
     public static final String COL_8 = "VIDEO_ID";
     public static final String COL_9 = "EVENT_TYPE";
 
+    public static final String MEMBER_TABLE = "tblMembers";
+    public static final String MCOL_1 = "MEMBER_ID";
+    public static final String MCOL_2 = "MEMBER_NAME";
+    public static final String MCOL_3 = "MEMBER_EMAIL";
+    public static final String MCOL_4 = "PASSWORD";
+    public static final String MCOL_5 = "STATUS";
+    public static final String MCOL_6 = "ADMIN";
+    public static final String MCOL_7 = "SIGNUP_DATE";
+    public static final String MCOL_8 = "LAST_LOGIN";
 
     public static final String TAG = "Result in DB";
     public static SQLiteDatabase db;
 
-    public DatabaseHelper(Context context) {
+    public static final String CREATE_TABLE="create table " + MEMBER_TABLE + "(MEMBER_ID INTEGER PRIMARY KEY AUTOINCREMENT, MEMBER_NAME TEXT, " +
+        " MEMBER_EMAIL TEXT, PASSWORD TEXT, STATUS BOOLEAN NOT NULL CHECK (STATUS IN (0,1)), ADMIN BOOLEAN "+
+        " NOT NULL CHECK (ADMIN IN (0,1)), SIGNUP_DATE DATETIME, LAST_LOGIN DATETIME)";
+
+
+    public DatabaseHelper(Context context)
+    {
         super(context,DATABASE_NAME,null,1);
     }
 
+    /*@Override
+    public void onOpen(SQLiteDatabase db) {
+        super.onOpen(db);
+        if (!db.isReadOnly()) {
+            // Enable foreign key constraints
+            db.execSQL("PRAGMA foreign_keys=ON;");
+        }
+    }
+*/
     @Override
     public void onCreate(SQLiteDatabase db) {
+        Log.i("helper","create table"+CREATE_TABLE);
 
-        //db.execSQL("drop table tblEventList");
+        db.execSQL("create table " + MEMBER_TABLE + "(MEMBER_ID INTEGER PRIMARY KEY AUTOINCREMENT, MEMBER_NAME TEXT, " +
+                " MEMBER_EMAIL TEXT, PASSWORD TEXT, STATUS BOOLEAN NOT NULL CHECK (STATUS IN (0,1)), ADMIN BOOLEAN "+
+                " NOT NULL CHECK (ADMIN IN (0,1)), SIGNUP_DATE DATETIME, LAST_LOGIN DATETIME)");
+
         db.execSQL("create table " + TABLE_NAME + "(ID INTEGER PRIMARY KEY AUTOINCREMENT, EVENT_NAME TEXT, CREATED_BY TEXT, RATING REAL, EVENT_URL TEXT, EVENT_DESC TEXT, EVENT_DATE DATETIME, VIDEO_ID TEXT, EVENT_TYPE TEXT)");
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists " + MEMBER_TABLE);
         db.execSQL("drop table if exists " + TABLE_NAME);
         onCreate(db);
+    }
+
+
+    public boolean insertMemberData(String member_name,String member_email, String password, int status,int admin,
+                              String signup_date, String last_login)
+    {
+        db=this.getWritableDatabase();
+        ContentValues cv=new ContentValues();
+        cv.put(MCOL_2, member_name);
+        cv.put(MCOL_3, member_email);
+        cv.put(MCOL_4, password);
+        cv.put(MCOL_5, status);
+        cv.put(MCOL_6, admin);
+        cv.put(MCOL_7, signup_date);
+        cv.put(MCOL_8, last_login);
+
+        long result = db.insert(MEMBER_TABLE, null, cv);
+        Log.i(TAG, "insert MEMBER Data: "+result);
+        if(result == -1)
+            return false;
+        else
+            return true;
+
+    }
+
+    public Cursor getMemberData()
+    {
+        db=this.getWritableDatabase();
+        Cursor res=db.rawQuery("select * from "+ MEMBER_TABLE,null);
+        Log.d("DB Helper", "get member Data: "+res);
+        return res;
+    }
+
+    public boolean updateMemberData(String member_id,String last_login)
+    {
+        db=this.getWritableDatabase();
+        ContentValues updateContentValues =new ContentValues();
+        updateContentValues.put(MCOL_1, member_id);
+        updateContentValues.put(MCOL_8, last_login);
+        db.update(MEMBER_TABLE, updateContentValues, "MEMBER_ID =?", new String[] {member_id});
+        return true;
     }
 
     public boolean insertData(String event_name,String created_by, String event_url, String event_desc,String event_date,
@@ -65,7 +136,6 @@ public class DatabaseHelper extends SQLiteOpenHelper
             return false;
         else
             return true;
-
     }
 
     public Cursor getAllData()
--- app/src/main/java/com/example/lenovo/eventapp/Home.java
+++ app/src/main/java/com/example/lenovo/eventapp/Home.java
@@ -1,17 +1,23 @@
 package com.example.lenovo.eventapp;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import java.util.Calendar;
 
 public class Home extends AppCompatActivity {
 
-    private static EditText email;
-    private static Button proceed_btn;
+    DatabaseHelper mydb;
+    //public static SQLiteDatabase db;
+    private  EditText email;
+    private  Button proceed_btn;
     // User Session Manager Class
     UserSessionManager session;
     int attempt_counter=5;
@@ -21,6 +27,7 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         // User Session Manager
+        mydb=new DatabaseHelper(this);
         session = new UserSessionManager(getApplicationContext());
 
         email= (EditText)findViewById(R.id.editText_email);
@@ -47,11 +54,126 @@ public class Home extends AppCompatActivity {
                 {
                     public void onClick(View v)
                     {
-                        if (email.getText().toString().equals("m.rajvel@gmail.com"))
+                        String substrName=email.getText().toString().substring(0,email.getText().toString().indexOf("@"));
+                        //String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
+                        Calendar c = Calendar.getInstance();
+                        //SimpleDateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+                        //Calendar c = Calendar.getInstance();
+                        Boolean updateMe = false;
+                        String memberEmail="";
+                        String memberName="";
+                        String memberId="";
+                        Boolean insertMe = false;
+
+                        String formattedDate = c.get(Calendar.YEAR) + "/"
+                                + c.get(Calendar.MONTH)
+                                + "/" + c.get(Calendar.DAY_OF_MONTH)
+                                + " " + c.get(Calendar.HOUR_OF_DAY)
+                                + ":" + c.get(Calendar.MINUTE);
+                        //String formattedDate = df.format(c.getTime());
+
+                        Cursor res = mydb.getMemberData();
+                        Log.i("table count","memebers" + res.getCount());
+
+                        if (res.getCount()== 0)
                         {
-                            /*Toast.makeText(Home.this,"Emil ID is correct",Toast.LENGTH_SHORT).show();
-                            Intent intent= new Intent("com.example.lenovo.eventapp.User");
-                            startActivity(intent);*/
+                            //mydb.onCreate(db);
+                            Log.i("table count","memebers 2" + res.getCount());
+                            Toast.makeText(Home.this,"No Data found",Toast.LENGTH_SHORT).show();
+                            boolean isMemberInserted = mydb.insertMemberData(substrName, email.getText().toString(), "", 1, 0, formattedDate, formattedDate);
+
+                            if (isMemberInserted == true)
+                            {
+                                Toast.makeText(Home.this, "Thank you for Signing up", Toast.LENGTH_SHORT).show();
+                                session.createUserLoginSession("EventApp", email.getText().toString());
+
+                                // Starting MainActivity
+                                Intent i = new Intent(getApplicationContext(), User.class);
+                                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+                                // Add new Flag to start new Activity
+                                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(i);
+
+                                finish();
+                            }
+                            else
+                            {
+                                Toast.makeText(Home.this, "Something went wrong!! Couldn't Signup", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        else
+                        {
+                            while (res.moveToNext())
+                            {
+                                Boolean emailMatch =  res.getString(2).equals(email.getText().toString());
+                                Log.i("emailMatch", String.valueOf(emailMatch));
+                               if (emailMatch)
+                               {
+                                   memberEmail = res.getString(2);
+                                   memberName = res.getString(1);
+                                   memberId = String.valueOf(res.getInt(0));
+                                   updateMe = true;
+                               }
+                                else
+                               {
+                                   Log.i("email doesn't exist", email.getText().toString());
+                                   memberEmail = email.getText().toString();
+                                   memberName = substrName;
+                                   insertMe = true;
+                               }
+                            }
+
+                        }
+
+                        if(updateMe == true)
+                        {
+                            boolean isMemberUpdated = mydb.updateMemberData(memberId,formattedDate);
+                            updateMe = false;
+                            if (isMemberUpdated == true)
+                                Toast.makeText(Home.this, "Welcome Back " + memberName, Toast.LENGTH_SHORT).show();
+                            else
+                                Toast.makeText(Home.this, "Member Data not Updated", Toast.LENGTH_SHORT).show();
+
+                            session.createUserLoginSession("EventApp", email.getText().toString());
+
+                            // Starting MainActivity
+                            Intent i = new Intent(getApplicationContext(), User.class);
+                            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            // Add new Flag to start new Activity
+                            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(i);
+                            finish();
+
+                        }
+                        else if (insertMe == true)
+                        {
+                            boolean isMemberInserted = mydb.insertMemberData(memberName,memberEmail, "", 1, 0, formattedDate, formattedDate);
+
+                            if (isMemberInserted == true)
+                            {
+                                Toast.makeText(Home.this, "Thank you for Signing up", Toast.LENGTH_SHORT).show();
+                                session.createUserLoginSession("EventApp", email.getText().toString());
+                                insertMe = false;
+                                // Starting MainActivity
+                                Intent i = new Intent(getApplicationContext(), User.class);
+                                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+                                // Add new Flag to start new Activity
+                                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(i);
+                                finish();
+                            }
+                            else
+                            {
+                                Toast.makeText(Home.this, "Something went wrong!! Couldn't Signup", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        /*if (email.getText().toString().equals("m.rajvel@gmail.com"))
+                        {
+                            Toast.makeText(Home.this,"Emil ID is correct",Toast.LENGTH_SHORT).show();
+                            //Intent intent= new Intent("com.example.lenovo.eventapp.User");
+                            //startActivity(intent);
                             // Creating user login session
                             // Statically storing name="Android Example"
                             // and email="androidexample84@gmail.com"
@@ -76,7 +198,7 @@ public class Home extends AppCompatActivity {
                             {
                                 proceed_btn.setEnabled(false);
                             }
-                        }
+                        }*/
 
                     }
 
